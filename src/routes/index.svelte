6a8a191a2e96ec98fe35eba5995a26b052855999
Arnab Ghosh <arnabcare21@gmail.com>
2022-08-03 12:38:47 -0600

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    const logOut = () => console.log("hello world!");
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
