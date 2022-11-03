09880e288023c11144aff9f2b138c453a320e3d2
Arnab Ghosh <arnabcare21@gmail.com>
2022-11-03 16:45:43 -0700

    Add some images to website, update about me description

---
+++ src/lib/Image.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let src: string;
+	export let alt: string;
+</script>
+
+
+<div class="p-3 m-3">
+    <img src={src} alt={alt} />
+</div>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,44 +1,46 @@
 <script lang="ts">
+	import Header from "../lib/Header.svelte";
+	import Image from "$lib/Image.svelte";
 
-import Header from "../lib/Header.svelte";
+	const paths = ['myface.jpg', 'ClubImages/img1.png', 'ClubImages/img2.png', 'ClubImages/img3.jpg', 'ClubImages/img4.jpg', 'ClubImages/img5.jpg'];
+
 </script>
 
-<Header> About </Header>
-<p class="">
-    Hello! I'm Arnab Ghosh, a High School student in Orange County, California. I'm very passionate about Technology.
-    I've been coding since middle school. I taught myself Python to work on a project for with a few friends, but my main
-    language quickly became JavaScript. Over High School, I've become a Novice at JavaScript/TypeScript. I learned a few web
-    frameworks, particularly React. I became a major contributor to 8by8, an organization dedicated to increasing voter turnout among AAPI
-    (Which is a cause that is very important to me, as a second generation immigrant from India).
-    For the AP Computer Science course, I familiarized myself with Java. I also learned things like Object Oriented Programming,
-    Object Oriented Design, and Algorithms/DS. For a few college courses I took over the summer, I learned the basics of C++ and dove deeper
-    into Algorithms and Data Structures. Recently, I've picked up Svelte as a framework and have started to create a few projects (including this website).
-    I've also been working on Bindr, a startup app that will allow students to consolidate assignments and courses across different LMS systems (this is not shown
-    because it is not finished), for which I've taken up to learning back-end web development, including REST API development, frameworks such as Express and Fastify,
-    and Databases (mainly MongoDB, and SQL Databases through Prisma). I'm still a Junior at High School, and I am always learning.
-</p>
-
-<div class="flex flex-row justify-between items-stretch">
-    <div class="text-left justify-self-start max-w-2xl">
-        <h2 class="text-2xl font-bold"> Experience </h2>
-        <h5 class="text-xl"><span class="font-bold"> theCoderSchool </span> - Code Coach (August 2021 - June 2022) </h5>
-        <p> Provided private tutoring services for theCoderSchool. Topics ranged from Scratch and Python to Unreal Engine and Unity Engine.
-            Emphasized fundamentals in code quality and conceptual understanding.
+<Header> About</Header>
+<div class="flex flex-row justify-center text-left grid-cols-3">
+    <ul class="display-block list-disc list-inside">
+        <p class="">
+            Hello! I'm Arnab Ghosh, a student passionate about technology and coding!
         </p>
-
-        <h5 class="text-xl"><span class="font-bold"> 8by8 </span> - Software Engineer (August 2021 - Present) </h5>
-        <p>
-            Major contributor for 8by8. Implemented Authentication and other major features via React, React Router, Firebase, and more.
-        </p>
-
-    </div>
-    <div class="text-right justify-self-end max-w-2xl">
-        <h2 class="text-2xl font-bold"> Skills </h2>
-        <h5 class="text-xl"><span class="font-bold"> Javascript/Typescript </span> - Expert </h5>
-        <h5 class="text-xl"><span class="font-bold"> React </span> - Intermediate </h5>
-        <h5 class="text-xl"><span class="font-bold"> Svelte </span> - Intermediate </h5>
-        <h5 class="text-xl"><span class="font-bold"> Firebase </span> - Intermediate </h5>
-        <h5 class="text-xl"><span class="font-bold"> MongoDB </span> - Intermediate </h5>
-
+        <li>
+            I'm a <span class="font-bold">Junior</span> at Aliso Niguel High School, in Aliso Viejo, California!
+        </li>
+        <li>
+            I have a <span class="font-bold">4.7</span> GPA!
+        </li>
+        <li>
+            I've taken <span class="font-bold">12</span> Honors, AP, or College Level Classes!
+        </li>
+        <li>
+            I have <span class="font-bold">3 years</span> of coding experience, mostly in JavaScript and TypeScript!
+        </li>
+        <li>
+            I have experience with <span class="font-bold">React, Svelte, and Express</span>, and have knowledge of other JavaScript frameworks!
+        </li>
+        <li>
+            I have miscellaneous experience with <span class="font-bold">Java, Kotlin, C++, and Python</span>, for a variety of reasons, including School!
+        </li>
+        <li>
+            I have tutored in <span class="font-bold">Math, English, Science, and Computer Science</span> to other students!
+        </li>
+    </ul>
+</div>
+<div class="flex flex-row justify-center text-left grid-cols-3">
+    <div class="container columns-3 ">
+        {#each paths as src}
+            <div class="">
+                <Image src={src} alt=""></Image>
+            </div>
+        {/each}
     </div>
 </div>
